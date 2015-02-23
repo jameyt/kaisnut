9b6f19a81a52261acdd612e1b1616705926c8ee3
James Tyler <jtyler@aaon.com>
2015-02-22 21:01:41 -0600

    scheduler: outputting schedule to webpage, not formatted

---
+++ scheduler.mvc/Controllers/ScheduleController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace scheduler.mvc.Controllers
+{
+    public class ScheduleController : Controller
+    {
+        // GET: Schedule
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Month(int id    )
+        {
+            var mockRepo = MockRepository.Create();
+            var schedule = Schedule.Create(mockRepo);
+
+            return View(schedule.Years[1].Months[id]);
+        }
+    }
+}
---
+++ scheduler.mvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="scheduler.mvc.MvcApplication" Language="C#" %>

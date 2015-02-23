d33714d89dce3c1459a649578ea856c633489c62
James Tyler <jtyler@aaon.com>
2015-02-23 14:16:52 -0600

    fizzbuzz kata

---
+++ FizzBuzz.Tests/Unit.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzz.Tests
+{
+    [TestClass]
+    public class Unit
+    {
+        [TestMethod]
+        public void Create()
+        {
+            var fizzBuzz = FizzBuzz.Create(100);
+        }
+
+        [TestMethod]
+        public void OutputMade()
+        {
+            var fizzBuzz = FizzBuzz.Create(100);
+            Assert.IsNotNull(fizzBuzz.Output);
+        }
+
+        [TestMethod]
+        public void OutputFilled()
+        {
+            var fizzBuzz = FizzBuzz.Create(100);
+            Assert.IsTrue(fizzBuzz.Output.Count == 100);
+        }
+
+        [TestMethod]
+        public void OutputsFizzOnce()
+        {
+
+            var fizzBuzz = FizzBuzz.Create(100);
+            Assert.AreEqual("fizz", fizzBuzz.Output[3]);
+        }
+
+        [TestMethod]
+        public void OutputsBuzzOnce()
+        {
+            var fizzBuzz = FizzBuzz.Create(100);
+            Assert.AreEqual("buzz", fizzBuzz.Output[5]);
+        }
+
+        [TestMethod]
+        public void OutputsFizzAll()
+        {
+            const int size = 100;
+            var fizzBuzz = FizzBuzz.Create(size);
+            for (var i = 0; i < size; i++)
+            {
+                if (i % 3 == 0 && i % 5 != 0)
+                {
+                    Assert.AreEqual("fizz", fizzBuzz.Output[i]);
+                }
+            }
+
+        }
+
+        [TestMethod]
+        public void OutputsBuzzAll()
+        {
+            const int size = 100;
+            var fizzBuzz = FizzBuzz.Create(size);
+            for (var i = 0; i < size; i++)
+            {
+                if (i % 5 == 0 && i % 3 != 0)
+                {
+                    Assert.AreEqual("buzz", fizzBuzz.Output[i]);
+                }
+            }
+        }
+    }
+}
---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class FizzBuzz
+    {
+        public List<string> Output { get; set; }
+
+        private FizzBuzz(int length)
+        {
+            Output = new List<string>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                var output = "";
+                if (i%3 == 0){output += "fizz";}
+                if (i % 5 == 0) { output += "buzz"; }
+               Output.Add(output);
+            }
+        }
+
+        public static FizzBuzz Create(int length)
+        {
+            return new FizzBuzz(length);
+        }
+    }
+}

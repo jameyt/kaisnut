7b4974a90ea0174d7c38ece688d6b86c3b9f671d
James Tyler <jtyler@aaon.com>
2015-02-27 21:49:02 -0600

    fibonacci: created solution and first unit test

---
+++ SumEvenFibonacciTo4Million.Tests/Unit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SumEvenFibonacciTo4Million.Tests
+{
+    [TestClass]
+    public class Unit
+    {
+        [TestMethod]
+        public void CreatesFibonacciSequenceTo10()
+        {
+            var expectedSequence = new List<int>() {1, 2, 3, 5, 8, 13, 21, 34, 55, 89};
+            var fibonacciSequence = Fibonacci.CreateSequence(10);
+
+        }
+    }
+}

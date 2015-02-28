92fa7a3d206b8e67d469a38909a8a64c73a1a77c
James Tyler <jtyler@aaon.com>
2015-02-27 23:25:32 -0600

    fibonacci: creates number, creates sequence, creates multiple sums

--- SumEvenFibonacciTo4Million.Tests/Unit.cs
+++ SumEvenFibonacciTo4Million.Tests/Unit.cs
@@ -12,7 +12,37 @@ namespace SumEvenFibonacciTo4Million.Tests
         {
             var expectedSequence = new List<int>() {1, 2, 3, 5, 8, 13, 21, 34, 55, 89};
             var fibonacciSequence = Fibonacci.CreateSequence(10);
+            for (var i = 0; i < expectedSequence.Count; i++)
+            {
+                Assert.AreEqual(expectedSequence[i],fibonacciSequence[i]);
+            }
+        }
 
+        [TestMethod]
+        public void Creates10thFibonacciNumber()
+        {
+            Assert.AreEqual(89,Fibonacci.CreateNumber(10));
+        }
+
+        [TestMethod]
+        public void Creates10thFibonacciNumberToSize()
+        {
+            var fibonacci = Fibonacci.CreateSequenceToSize(89);
+            Assert.AreEqual(89,fibonacci[fibonacci.Count-1] );
+        }
+
+        [TestMethod]
+        public void SumsEvensTo89()
+        {
+            var fibonacci = Fibonacci.SumFibonacciMultiples(89,2);
+            Assert.AreEqual(44, fibonacci);
+        }
+
+        [TestMethod]
+        public void SumsEvensTo4000000()
+        {
+            var fibonacci = Fibonacci.SumFibonacciMultiples(4000000,2);
+            Assert.AreEqual(4613732, fibonacci);
         }
     }
 }
---
+++ SumEvenFibonacciTo4Million/Fibonacci.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumEvenFibonacciTo4Million
+{
+    public class Fibonacci
+    {
+        private Fibonacci() { }
+
+        public Fibonacci Create() { return new Fibonacci(); }
+
+        public static int CreateNumber(int length)
+        {
+            switch (length)
+            {
+                case 1:
+                    return 1;
+                case 2:
+                    return 2;
+                default:
+                    return CreateNumber(length - 1) + CreateNumber(length - 2);
+            }
+        }
+
+        private static List<int> _sequence;
+
+        public static  List<int> CreateSequence(int length)
+        {
+            if (_sequence == null)
+            {
+                _sequence = new List<int> {1, 2};
+            }
+
+            while (_sequence.Count < length)
+            {
+                _sequence.Add(_sequence[_sequence.Count-1]+_sequence[_sequence.Count-2]);
+            }
+
+            return _sequence;
+        }
+
+        public static List<int> CreateSequenceToSize(int size)
+        {
+            if (_sequence == null)
+            {
+                _sequence = new List<int> { 1, 2 };
+            }
+
+            while (_sequence[_sequence.Count - 1] <= size)
+            {
+                var nextNumber = _sequence[_sequence.Count - 1] + _sequence[_sequence.Count - 2];
+                if (nextNumber > size) { break;}
+                _sequence.Add(nextNumber);
+            } 
+
+            return _sequence;
+        }
+
+        public static int SumFibonacciMultiples(int size,int multiple)
+        {
+            var sequence = CreateSequenceToSize(size);
+
+            return sequence.Where(number => number%multiple == 0).Where(number => number<=size).Sum();
+        }
+
+    }
+}

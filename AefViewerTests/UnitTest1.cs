f4694f15a9eb10d443b98f3ec0292db96f1a5749
James Tyler <jtyler@aaon.com>
2015-04-04 10:45:26 -0500

    ecat file viewer in wrong repo

---
+++ AefViewerTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using AAON.Ecat.Orders.Data.Objects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AefViewerTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CreateConnection()
+        {
+            var dataTables = AaonEcatFile.ImportOrder(@"C:\Users\tyler-eg\Desktop\WRL Design.aef");
+            var aef = AaonEcatFile.Create(dataTables);
+            
+        }
+    }
+}

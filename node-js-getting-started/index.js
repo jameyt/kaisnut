19b4fa5905e8b06c3f74131b25b1802a10683117
Jamey Tyler <jameyerictyler@gmail.com>
2015-04-03 21:12:05 -0500

    node-js-getting-started working as james games, deleted james games folder and starting over

--- node-js-getting-started/index.js
+++ node-js-getting-started/index.js
@@ -13,11 +13,15 @@
 //   console.log("Node app is running at localhost:" + app.get('port'));
 // });
 
-var express = require('express')
-var app = express();
-var cool = require('cool-ascii-faces');
+//start here
 
-app.set('port', (process.env.PORT || 5000))
+// var express = require('express')
+// var app = express();
+// var cool = require('cool-ascii-faces');
+
+// app.set('port', (process.env.PORT || 5000))
+
+//end here
 
 // app.get('/', function(request, response) {
 //   response.send(cool());
@@ -31,20 +35,31 @@ app.set('port', (process.env.PORT || 5000))
 //   response.send(result);
 // });
 
-var pg = require('pg');
+//start here
 
-app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.send(result.rows); }
-    });
-  });
-})
+// var pg = require('pg');
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+// app.get('/db', function (request, response) {
+//   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+//     client.query('SELECT * FROM test_table', function(err, result) {
+//       done();
+//       if (err)
+//        { console.error(err); response.send("Error " + err); }
+//       else
+//        { response.send(result.rows); }
+//     });
+//   });
+// })
+
+// app.listen(app.get('port'), function() {
+//   console.log("Node app is running at localhost:" + app.get('port'))
+// })
+
+//end here
+
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(process.env.PORT || 3000);

07dfee7ea82269b2d2c93a6435f97fbacd599de3
Jamey Tyler <jameyerictyler@gmail.com>
2015-04-24 22:40:14 -0500

    scheduler: schedule looking like a calendar, more playing with node apps

---
+++ scheduler/scheduler.mvc/Controllers/DetailedScheduleController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using scheduler.data;
+
+namespace scheduler.mvc.Controllers
+{
+    public class DetailedScheduleController : Controller
+    {
+        // GET: Schedule
+        public ActionResult Index(int? month, int? day, int? year)
+        {
+            DateTime date;
+            if (!month.HasValue || !day.HasValue || year.HasValue){date = DateTime.Now;}
+            else{date = new DateTime(year.Value,month.Value,day.Value);}
+            
+            var repo = Repository.Create();
+
+            var detailedSchedule = repo.GetDetailedSchedule(date);
+
+            return View(detailedSchedule);
+        }
+
+        public ActionResult Edit(int? month, int? day, int? year)
+        {
+            DateTime date;
+            if (!month.HasValue || !day.HasValue || year.HasValue) { date = DateTime.Now; }
+            else { date = new DateTime(year.Value, month.Value, day.Value); }
+
+            var repo = Repository.Create();
+
+            var detailedSchedule = repo.GetDetailedSchedule(date);
+
+            return View(detailedSchedule);
+        }
+
+    }
+}
